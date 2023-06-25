f0bff87458ea8e77eac3e67c7b8c76e9578151af
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2023-06-25 16:10:01 +0600

    Add create_user_project_table migration

---
+++ database/migrations/2023_06_25_100750_create_user_project_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_project', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('workspace_role_id')->references('id')->on('workspace_roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_project');
+    }
+};
